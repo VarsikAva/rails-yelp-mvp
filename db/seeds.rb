80d9baf1c8fe113ba2b4dbffe6cc816f8897565b
Varsik Orchanian <varsenik.ava@gmail.com>
2024-09-25 14:57:16 +0200

    Yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Restaurant.destroy_all
+
+restaurants = [
+  { name: "Epicure", address: "75008 Paris", phone_number: "0123456789", category: "french" },
+  { name: "La Familia", address: "75010 Paris", phone_number: "0987654321", category: "italian" },
+  { name: "Sushi Shop", address: "75011 Paris", phone_number: "0147258369", category: "japanese" },
+  { name: "Peking", address: "75012 Paris", phone_number: "0147258368", category: "chinese" },
+  { name: "Bruxelles", address: "75013 Paris", phone_number: "0147258367", category: "belgian" }
+]
+
+restaurants.each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
